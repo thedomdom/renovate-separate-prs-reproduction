17d516df9a02d8fcaf2182834c1a58dbbec5b897
Dominik Wunderlich <dominik.wunderlich@sap.com>
2023-08-01 15:22:18 +0200

    renovate reproduction

---
+++ echoer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+IMAGE_VERSION_DOCTYPE1="3.8"
+IMAGE_VERSION_DOCTYPE2="3.8"
+
+docker run python:$IMAGE_VERSION_DOCTYPE1 python -c "print('I can process doctype 1 very well!')"
+docker run python:$IMAGE_VERSION_DOCTYPE2 python -c "print('I can process doctype 2 very well!')"
